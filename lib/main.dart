bacaef65155635c6ac9423b64a09ba70120f6261
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-12-30 12:50:26 -0300

    03.03 - Estrutura de código

--- lib/main.dart
+++ lib/main.dart
@@ -9,55 +9,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepOrange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        body: SizedBox(
-          height: 250.0,
-          child: Card(
-            margin: EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Image.network(
-                      'https://amp.receitadevovo.com.br/wp-content/uploads/2020/10/bolo-de-trigo-fofinho.jpg',
-                      fit: BoxFit.fill,
-                      height: 218.0,
-                    ),
-                    Positioned(
-                      bottom: 10.0,
-                      left: 10.0,
-                      child: Text(
-                        'Bolo de Laranja',
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.deepOrange,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _construirHome();
+  }
+
+  Widget _construirHome() {
+    return Scaffold(
+      body: _construirCard(),
+      appBar: _construirAppBar(),
+    );
+  }
+
+  Widget _construirCard() {
+    return SizedBox(
+      height: 250.0,
+      child: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Stack(
+              children: [_construirImagemCard(), _construirTextoCard()],
             ),
-          ),
-        ),
-        appBar: AppBar(
-          title: Text('Cozinhando em Casa'),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _construirAppBar() {
+    return AppBar(
+      title: Text('Cozinhando em Casa'),
+    );
+  }
+
+  Widget _construirTextoCard() {
+    return Positioned(
+      bottom: 10.0,
+      left: 10.0,
+      child: Text(
+        'Bolo de Laranja',
+        style: TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+
+  Widget _construirImagemCard() {
+    return Image.network(
+      'https://amp.receitadevovo.com.br/wp-content/uploads/2020/10/bolo-de-trigo-fofinho.jpg',
+      fit: BoxFit.fill,
+      height: 218.0,
     );
   }
 }
